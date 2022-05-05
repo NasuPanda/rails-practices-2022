8991e1bc4a19d6fcbd8318c3552b795c0809223e
Westen0511 <xiyuanjunzhi@gmail.com>
2022-05-05 09:06:04 +0900

    コントローラの追加
    フックの追加

---
+++ perfect-RoR/book_admin/app/controllers/books_controller.rb
@@ -0,0 +1,26 @@
+class BooksController < ApplicationController
+  # forgery: 偽造と言う意味。CSRF対策。
+  protect_from_forgery except: [:destroy]
+  before_action :set_book, only: [:show, :destroy]
+
+  def show
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
+  def destroy
+    @book.destroy
+    respond_to do |format|
+      format.html { redirect_to '/' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+    def set_book
+      @book = Book.find(params[:id])
+    end
+end
--- perfect-RoR/book_admin/app/models/book.rb
+++ perfect-RoR/book_admin/app/models/book.rb
@@ -1,4 +1,10 @@
 class Book < ApplicationRecord
+  # Enumの定義
+  enum sales_status: {
+    reservation: 0,   # 予約受付
+    now_on_sale: 1,   # 発売中
+    end_of_print: 2,  # 販売終了
+  }
   scope :costly, -> { where('price > ?', 3000) }
   scope :written_about, ->(theme) { where("name like ?", "%#{theme}%") }
 
@@ -12,10 +18,4 @@ class Book < ApplicationRecord
   validates :name, presence: true
   validates :name, length: { maximum: 25 }
   validates :price, numericality: { greater_than_or_equal_to: 0 }
-  # 独自バリデーション
-  validate do |book|
-    if book.name.include?("exercise")
-      book.errors[:name] << "I don't like exercise"
-    end
-  end
 end
