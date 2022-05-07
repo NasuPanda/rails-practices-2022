9623265dda8dc87fa3d298a4b1726ae7ac1907a8
Westen0511 <xiyuanjunzhi@gmail.com>
2022-05-08 07:13:21 +0900

    退会機能の実装

--- perfect-RoR/awesome_events/config/routes.rb
+++ perfect-RoR/awesome_events/config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   resources :events do
     resources :tickets
   end
+
+  resource :retirements
+
   root 'welcome#index'
   get 'auth/:provider/callback' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
