587c238b737fc9a683288fb79f92835b57be7f2a
Westen0511 <xiyuanjunzhi@gmail.com>
2022-05-08 07:00:14 +0900

    Fix ルーティング周りのエラー修正

--- perfect-RoR/awesome_events/app/controllers/tickets_controller.rb
+++ perfect-RoR/awesome_events/app/controllers/tickets_controller.rb
@@ -5,12 +5,14 @@ class TicketsController < ApplicationController
 
   def create
     event = Event.find(params[:event_id])
+    puts params
     @ticket = current_user.tickets.build do |t|
       t.event = event
-      t.comment = params[:ticket][:comment]
+      t.comment = params[:comment]
     end
     if @ticket.save
       redirect_to event, notice: "このイベントに参加表明しました"
+    end
   end
 
   def destroy
