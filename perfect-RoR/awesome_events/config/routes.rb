6ed8be9da6a4cba58b1dd27abf808e570a8352fd
Westen0511 <xiyuanjunzhi@gmail.com>
2022-05-08 13:27:36 +0900

    rescue_fromを利用したエラーハンドリングの追加

--- perfect-RoR/awesome_events/config/routes.rb
+++ perfect-RoR/awesome_events/config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
+  get 'auth/:provider/callback' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
   # リソースの親子関係
   resources :events do
     resources :tickets
   end
-
   resource :retirements
 
-  root 'welcome#index'
-  get 'auth/:provider/callback' => 'sessions#create'
-  delete '/logout' => 'sessions#destroy'
+  # 全てのURLをキャッチする(存在しないURLへのリクエストをキャッチ)
+  match "*path" => "application#error404", via: :all
 end
