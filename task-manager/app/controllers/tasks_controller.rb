72d6dcf14b172ca992443c6fa044090de254dbce
Westen0511 <xiyuanjunzhi@gmail.com>
2022-05-10 08:20:18 +0900

    tasksコントローラの追加

---
+++ task-manager/app/controllers/tasks_controller.rb
@@ -0,0 +1,22 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  private
+    def task_params
+      params.require(:task).permit(:name, :description, :due_on, :completed)
+    end
+end
