8f5d5551118620257e9d5c3ddc2646c98c24dfb7
Westen0511 <xiyuanjunzhi@gmail.com>
2022-05-09 20:58:30 +0900

    Userモデルテストの追加

--- task-manager/spec/models/user_spec.rb
+++ task-manager/spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let (:user) { FactoryBot.build(:user) }
+
+  context "有効なユーザ" do
+    it "有効であること" do
+      expect(user).to be_valid
+    end
+  end
+
+  context "無効なユーザ" do
+    it "メールアドレスが無ければ無効なこと" do
+      user.email = ""
+      expect(user).to_not be_valid
+    end
+
+    it "パスワードが無ければ無効なこと" do
+      user.password = ""
+      expect(user).to_not be_valid
+    end
+  end
 end
