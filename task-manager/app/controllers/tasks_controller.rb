910f44427d41fdc82df8dedb3bc9a19db3cef213
Westen0511 <xiyuanjunzhi@gmail.com>
2022-05-10 21:02:03 +0900

    create, updateの実装

--- task-manager/app/controllers/tasks_controller.rb
+++ task-manager/app/controllers/tasks_controller.rb
@@ -1,4 +1,6 @@
 class TasksController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @tasks = Task.all
   end
@@ -13,6 +15,30 @@ class TasksController < ApplicationController
 
   def edit
     @task = Task.find(params[:id])
+  end
+
+  def create
+    task = Task.new(task_params)
+    # completedを真偽値にキャストする
+    task.completed = ActiveRecord::Type::Boolean.new.cast(task.completed)
+    task.user_id = current_user.id
+    if task.save
+      redirect_to task
+      flash[:notice] = "タスクの作成に成功しました"
+    else
+      render 'new'
+      flash[:alert] = "タスクの作成に失敗しました"
+    end
+  end
+
+  def update
+    task = Task.find(params[:id])
+    if task.update(task_params)
+      flash[:success] = "タスクを編集しました"
+      redirect_to task
+    else
+      render 'edit'
+    end
   end
 
   private
