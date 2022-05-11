b14c90fcb6a315679b62148ae07751f529672b88
Westen0511 <xiyuanjunzhi@gmail.com>
2022-05-12 08:16:11 +0900

    feat: Bootstrap4.4→5に更新

--- task-manager/app/javascript/packs/application.js
+++ task-manager/app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("turbolinks").start()
 // Bootstrap導入
 import "bootstrap"
 import "bootstrap/scss/bootstrap.scss"
+import "../stylesheets/application.scss"
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
