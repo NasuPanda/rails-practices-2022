0da9603c9af215faa6bc59e82ddb37acd9c22801
Westen0511 <xiyuanjunzhi@gmail.com>
2022-05-11 19:20:33 +0900

    test: リクエストスペックの追加/非ログインユーザーとして

--- task-manager/spec/requests/tasks_spec.rb
+++ task-manager/spec/requests/tasks_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Tasks", type: :request do
   let(:task) { FactoryBot.create(:task) }
+  let(:valid_params) { FactoryBot.attributes_for(:task) }
 
   context "ログインユーザーとして" do
     before do
@@ -50,8 +51,6 @@ RSpec.describe "Tasks", type: :request do
     end
 
     context "有効なパラメータ" do
-      let(:valid_params) { FactoryBot.attributes_for(:task) }
-
       describe "POST /tasks" do
         it "タスクが作成されること" do
           expect {
@@ -160,6 +159,71 @@ RSpec.describe "Tasks", type: :request do
             }
             expect(response).to redirect_to edit_task_path
         end
+      end
+    end
+  end
+
+  context "非ログインユーザーとして" do
+    describe "GET /tasks" do
+      it "/users/sign_in にリダイレクトされること" do
+        get tasks_path
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    describe "GET /tasks/:id" do
+      it "/users/sign_in にリダイレクトされること" do
+        get task_path(task)
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    describe "GET /tasks/new" do
+      it "/users/sign_in にリダイレクトされること" do
+        get new_task_path
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    describe "GET /tasks/:id/edit" do
+      it "/users/sign_in にリダイレクトされること" do
+        get edit_task_path(task)
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    describe "DELETE /tasks/:id" do
+      it "/users/sign_in にリダイレクトされること" do
+        delete task_path(task)
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    describe "POST /tasks" do
+      it "/users/sign_in にリダイレクトされること" do
+        post tasks_path,
+          params: {
+            task: {
+              name: valid_params[:name],
+              description: valid_params[:description],
+              due_on: valid_params[:due_on],
+              completed: valid_params[:completed],
+              user_id: task.user_id
+            }
+          }
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+
+    describe "PATCH /tasks/:id" do
+      it "/users/sign_in にリダイレクトされること" do
+        patch task_path(task),
+          params: {
+            task: {
+              name: "New Name"
+            }
+          }
+        expect(response).to redirect_to new_user_session_path
       end
     end
   end
